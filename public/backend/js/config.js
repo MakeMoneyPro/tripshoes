4695bc46f9b241bfa445afdce27831d32fd17b7f
vietlq57 <luongviet82@gmail.com>
2017-05-26 22:32:58 +0700

    upload image blog

--- public/backend/js/config.js
+++ public/backend/js/config.js
@@ -1,9 +1,9 @@
 CKEDITOR.replace('content', {
     language: 'en',
-    filebrowserBrowseUrl: 'ckfinder/ckfinder.html',
-    filebrowserImageBrowseUrl: 'ckfinder/ckfinder.html?type=Images',
-    filebrowserFlashBrowseUrl: 'ckfinder/ckfinder.html?type=Flash',
-    filebrowserUploadUrl: 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserImageUploadUrl: 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-    filebrowserFlashUploadUrl: 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+    filebrowserBrowseUrl: 'http://localhost/tripshoes/public/backend/js/ckfinder/ckfinder.html',
+    filebrowserImageBrowseUrl: 'http://localhost/tripshoes/public/backend/js/ckfinder/ckfinder.html?type=Images',
+    filebrowserFlashBrowseUrl: 'http://localhost/tripshoes/public/backend/js/ckfinder/ckfinder.html?type=Flash',
+    filebrowserUploadUrl: 'http://localhost/tripshoes/public/backend/js/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+    filebrowserImageUploadUrl: 'http://localhost/tripshoes/public/backend/js/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+    filebrowserFlashUploadUrl: 'http://localhost/tripshoes/public/backend/js/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
 });
